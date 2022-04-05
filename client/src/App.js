cf1a768a5b68c960179a97a26ab21f27fde5ff0b
Mathias Wagner <germannewsmaker@gmail.com>
2022-04-05 19:37:31 +0200

    Created the App.js of the client (͡° ͜ʖ ͡°)

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            {/* TODO: Start coding, i guess */}
+        </div>
+    );
+}
+
+export default App;
