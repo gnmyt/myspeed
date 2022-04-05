fb593c2b9ea934eb25cc5eaa29ce27409224b836
Mathias Wagner <germannewsmaker@gmail.com>
2022-04-05 20:01:35 +0200

    Added some server code to the index.js

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.port || 5216;
+
+// Register middlewares
+app.use(express.json());
+
+
+// Enable production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../build')));
+
+    app.get('*', function(req, res) {
+        res.sendFile(path.join(__dirname, '../build', 'index.html'));
+    });
+}
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
