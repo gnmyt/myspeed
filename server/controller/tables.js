08e71b4390872a3143f475e3f9573865526f0d79
Mathias Wagner <germannewsmaker@gmail.com>
2022-04-06 20:43:58 +0200

    MySpeed now automatically creates both tables & inserts the default values into the config table

---
+++ server/controller/tables.js
@@ -0,0 +1,32 @@
+const db = require('../index').database;
+
+const configDefaults = {
+    "setupDone": "true",
+    "ping": 25,
+    "download": 100,
+    "upload": 50,
+    "serverId": "none",
+    "password": "none"
+}
+
+module.exports.create = () => {
+    db.exec("create table if not exists config(" +
+        "    id    integer primary key autoincrement," +
+        "    key   varchar(10)," +
+        "    value varchar(255));");
+    db.exec("create table if not exists speedtests(" +
+        "    id       integer primary key autoincrement," +
+        "    ping     integer(5000)," +
+        "    download double," +
+        "    upload double," +
+        "    created DATETIME DEFAULT CURRENT_TIMESTAMP);");
+}
+
+module.exports.insert = () => {
+    if (db.prepare("SELECT value FROM config WHERE key = ?").get("setupDone")) return;
+
+    let statement = db.prepare("INSERT INTO config (key, value) VALUES (?, ?)");
+    for (let key in configDefaults) {
+        statement.run(key, configDefaults[key]);
+    }
+}
