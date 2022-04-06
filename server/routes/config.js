7f26ae454334605b76f8c804525942185cc6c6f1
Mathias Wagner <germannewsmaker@gmail.com>
2022-04-06 21:56:21 +0200

    Added a number security check to the PATCH /config/:key route

--- server/routes/config.js
+++ server/routes/config.js
@@ -22,6 +22,9 @@ app.get("/:key", (req, res) => {
 app.patch("/:key", async (req, res) => {
     if (!req.body.value) return res.status(400).json({message: "You need to provide the new value"});
 
+    if ((req.params.key === "ping" || req.params.key === "download" || req.params.key === "upload") && isNaN(req.body.value))
+        return res.status(400).json({message: "You need to provide a number in order to change this"});
+
     if (req.params.key === "password" && req.body.value !== "none") req.body.value = await require('bcrypt').hash(req.body.value, 10);
 
     if (!config.update(req.params.key, req.body.value)) return res.status(404).json({message: "The provided key does not exist"});
