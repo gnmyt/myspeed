98a129216b460e435acfb2d91617c5465ebf4a04
Mathias Wagner <germannewsmaker@gmail.com>
2022-04-06 15:01:46 +0200

    Updated the production logic

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../build')));
 
-    app.get('*', function(req, res) {
+    app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '../build', 'index.html'));
     });
 }
