dc9e5c03ab91451762b608ff743be3bcfc27efb2
Mathias Wagner <germannewsmaker@gmail.com>
2022-04-07 18:58:32 +0200

    Created the speedtest creation logic

---
+++ server/tasks/speedtest.js
@@ -0,0 +1,34 @@
+const speedTest = require('speedtest-net');
+const tests = require('../controller/speedtests');
+const config = require('../controller/config');
+
+function roundSpeed(bytes, elapsed) {
+    return Math.round((bytes * 8 / elapsed) / 10) / 100;
+}
+
+module.exports.run = async () => {
+    let serverId = config.get("serverId").value;
+    if (serverId === "none")
+        serverId = undefined;
+
+    let speedtest = await speedTest({acceptLicense: true, acceptGdpr: true, serverId: serverId});
+
+    if (serverId === undefined)
+        config.update("serverId", speedtest.server.id);
+
+    return speedtest;
+}
+
+module.exports.create = () => {
+    this.run().then(test => {
+        let ping = Math.round(test.ping.latency);
+        let download = roundSpeed(test.download.bytes, test.download.elapsed);
+        let upload = roundSpeed(test.upload.bytes, test.upload.elapsed);
+        let testResult = tests.create(ping, download, upload);
+        console.log(`Test #${testResult} was executed successfully. 🏓 ${ping} ⬇ ${download}️ ⬆ ${upload}️`);
+    });
+}
+
+module.exports.removeOld = () => {
+    tests.removeOld();
+}
