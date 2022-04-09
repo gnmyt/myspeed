e63c0c322250514f0082927b423839b32ea36224
Mathias Wagner <germannewsmaker@gmail.com>
2022-04-09 23:16:09 +0200

    Created the config context

---
+++ client/src/context/ConfigContext.js
@@ -0,0 +1,37 @@
+import React, {useState, createContext, useEffect, useContext} from "react";
+import {DialogContext} from "./DialogContext";
+
+export const ConfigContext = createContext();
+
+export const ConfigProvider = (props) => {
+
+    const [config, setConfig] = useState({});
+    const [setDialog] = useContext(DialogContext);
+
+
+    useEffect(() => {
+        let passwordHeaders = localStorage.getItem("password") ? {password: localStorage.getItem("password")} : {}
+        fetch("/api/config", {headers: passwordHeaders})
+            .then(res => {
+                if (!res.ok) throw new Error();
+                return res;
+            })
+            .then(res => res.json())
+            .then(result => setConfig(result))
+            .catch(() => setDialog({
+                title: "Passwort erforderlich",
+                placeholder: "Dein Passwort",
+                onClose: () => window.location.reload(),
+                onSuccess: (value) => {
+                    localStorage.setItem("password", value);
+                    window.location.reload();
+                }
+            }));
+    }, [setConfig, setDialog]);
+
+    return (
+        <ConfigContext.Provider value={config}>
+            {props.children}
+        </ConfigContext.Provider>
+    )
+}
