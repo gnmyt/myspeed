6bdab5df6d10ea8178ba38c73910f3d1dd1b7fef
Mathias Wagner <germannewsmaker@gmail.com>
2022-05-07 22:35:44 +0200

    Rounded the values in the recommendations.js controller

--- server/controller/recommendations.js
+++ server/controller/recommendations.js
@@ -8,8 +8,10 @@ module.exports.get = () => {
 // Sets new recommendations
 module.exports.set = (ping, download, upload) => {
     if (this.get() === undefined) {
-        return db.prepare("INSERT INTO recommendations (ping, download, upload) VALUES (?, ?, ?)").run(ping, download, upload);
+        return db.prepare("INSERT INTO recommendations (ping, download, upload) VALUES (?, ?, ?)")
+            .run(Math.round(ping), Math.round(download), Math.round(upload));
     } else {
-        return db.prepare("UPDATE recommendations SET ping = ?, download = ?, upload = ?").run(ping, download, upload);
+        return db.prepare("UPDATE recommendations SET ping = ?, download = ?, upload = ?")
+            .run(Math.round(ping), Math.round(download), Math.round(upload));
     }
 }
