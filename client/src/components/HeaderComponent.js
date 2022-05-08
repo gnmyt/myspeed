e0b162cb3863d5c226d7a967544670701830914e
Mathias Wagner <germannewsmaker@gmail.com>
2022-05-08 02:06:30 +0200

    Created the icon state in the HeaderComponent to switch the icon of the dropdown

--- client/src/components/HeaderComponent.js
+++ client/src/components/HeaderComponent.js
@@ -1,22 +1,26 @@
 import "../style/Header.sass";
-import {Component} from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faGear} from "@fortawesome/free-solid-svg-icons";
 import DropdownComponent, {toggleDropdown} from "./DropdownComponent";
+import {useState} from "react";
 
-class HeaderComponent extends Component {
+function HeaderComponent() {
 
-    render() {
-        return (
-            <header>
-                <div className="header-main">
-                    <h2>Netzwerkanalyse</h2>
-                    <FontAwesomeIcon icon={faGear} className="settings" onClick={toggleDropdown}/>
-                </div>
-                <DropdownComponent/>
-            </header>
-        )
+    const [icon, setIcon] = useState(faGear);
+
+    function switchDropdown() {
+        toggleDropdown(setIcon);
     }
+
+    return (
+        <header>
+            <div className="header-main">
+                <h2>Netzwerkanalyse</h2>
+                <FontAwesomeIcon icon={icon} className="settings" onClick={switchDropdown}/>
+            </div>
+            <DropdownComponent/>
+        </header>
+    )
 
 }
 
