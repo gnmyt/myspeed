7886bf9048a1d021c7d0a25f9b9dbde65d1376bc
Mathias Wagner <germannewsmaker@gmail.com>
2022-05-09 23:13:04 +0200

    Fixed the button text in the password dialog

--- client/src/context/ConfigContext.js
+++ client/src/context/ConfigContext.js
@@ -23,6 +23,7 @@ export const ConfigProvider = (props) => {
                 placeholder: "Dein Passwort",
                 description: localStorage.getItem("password") ? <span className="icon-red">Das von dir eingegebene Passwort ist falsch</span> : "",
                 password: true,
+                buttonText: "Fertig",
                 onClose: () => window.location.reload(),
                 onSuccess: (value) => {
                     localStorage.setItem("password", value);
