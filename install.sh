ddf46219ee9757f2c3022dc69e629475184eeca1
Paul Schwarz <36771750+pavl21@users.noreply.github.com>
2022-05-10 20:10:53 +0200

    Installationsprozess vereinfacht
    Der Prozess wurde von der Sicht des Nutzers komplett überarbeitet. Man bekommt jetzt mehr Feedback, was genau gerade passiert. Einige Sonderzeichen (Unicodes) wurden hinzugefügt und die Hinweise wurden überarbeitet.

--- install.sh
+++ install.sh
@@ -17,16 +17,13 @@ log () {
 
 # Root check
 if [ $EUID -ne 0 ]; then
-  echo "$RED ✗ Fehler bei der Installation: $NORMAL Du benötigst Root-Rechte, um die Installation zu starten."
-  echo "$BLUE Die Root-Abfrage wird ausgeführt, bitte gib das dazugehörige Passwort ein. Die Installation wird dann automatisch neu gestartet. Um abzubrechen, drücke STRG+C."
-  sudo -s
-  curl -sSL https://raw.githubusercontent.com/gnmyt/myspeed/release/install.sh | bash
+  echo -e "$RED ✗ Fehler bei der Installation: $NORMAL Du benötigst Root-Rechte, um die Installation zu starten."
   exit
 fi
 
 # Check if installed
 if [ -d $INSTALLATION_PATH ]; then
-    echo "$YELLOW ⚠ Fehler bei der Installation: $NORMAL MySpeed ist bereits auf diesem System installiert. ($INSTALLATION_PATH)"
+    echo -e "$YELLOW ⚠ Fehler bei der Installation: $NORMAL MySpeed ist bereits auf diesem System installiert. ($INSTALLATION_PATH)"
     exit 0
 fi
 
@@ -35,76 +32,76 @@ fi
 apt-get update -y
 
 clear
-echo "$GREENℹ Info:$NORMAL Die Installation wird jetzt vorbereitet. Das kann einen Augenblick dauern..."
+echo -e "$GREENℹ Info:$NORMAL Die Installation wird jetzt vorbereitet. Das kann einen Augenblick dauern..."
 sleep 5
 # Check for wget
 clear
-echo "$BLUE🔎 Status:$NORMAL Überprüfe, ob wget vorhanden ist..."
+echo -e "$BLUE🔎 Status:$NORMAL Überprüfe, ob wget vorhanden ist..."
 if ! command -v wget &> /dev/null
 then
-    echo "$YELLOWℹ\"wget\" ist nicht installiert.$NORMAL Die Installation wurde gestartet..."
+    echo -e "$YELLOWℹ\"wget\" ist nicht installiert.$NORMAL Die Installation wurde gestartet..."
     sleep 2
     apt-get install wget -y
 fi
 
 # Check for unzip
 clear
-echo "$BLUE🔎 Status:$NORMAL Überprüfe, ob unzip vorhanden ist..."
+echo -e "$BLUE🔎 Status:$NORMAL Überprüfe, ob unzip vorhanden ist..."
 if ! command -v unzip &> /dev/null
 then
-    echo "$YELLOWℹ\"unzip\" ist nicht installiert.$NORMAL Die Installation wurde gestartet..."
+    echo -e "$YELLOWℹ\"unzip\" ist nicht installiert.$NORMAL Die Installation wurde gestartet..."
     sleep 2
     apt-get install unzip -y
 fi
 
 # Check for curl
 clear
-echo "$BLUE 🔎 Status:$NORMAL Überprüfe, ob curl vorhanden ist..."
+echo -e "$BLUE 🔎 Status:$NORMAL Überprüfe, ob curl vorhanden ist..."
 if ! command -v curl &> /dev/null
 then
-    echo "$YELLOWℹ\"curl\" ist nicht installiert.$NORMAL Die Installation wurde gestartet..."
+    echo -e "$YELLOWℹ\"curl\" ist nicht installiert.$NORMAL Die Installation wurde gestartet..."
     sleep 2
     apt-get install curl -y
 fi
 
 # Check for node
 clear
-echo "$BLUE 🔎 Status:$NORMAL Überprüfe, ob node vorhanden ist..."
+echo -e "$BLUE 🔎 Status:$NORMAL Überprüfe, ob node vorhanden ist..."
 if ! command -v node &> /dev/null
 then
-    echo "$YELLOWℹ\"node\" ist nicht installiert.$NORMAL Die Installation wurde gestartet..."
+    echo -e "$YELLOWℹ\"node\" ist nicht installiert.$NORMAL Die Installation wurde gestartet..."
     sleep 2
     curl -sSL https://deb.nodesource.com/setup_16.x | bash
     apt-get install nodejs -y
 fi
 
 clear
-echo "$GREEN✓ Vorbereitung abgeschlossen:$NORMAL Die Installation von MySpeed wird jetzt gestartet..."
+echo -e "$GREEN✓ Vorbereitung abgeschlossen:$NORMAL Die Installation von MySpeed wird jetzt gestartet..."
 sleep 5
 
 clear
 if [ ! -d $INSTALLATION_PATH ]
 then
-    echo "$BLUEℹ Info: $NORMAL MySpeed wird unter dem Verzeichnis $INSTALLATION_PATH installiert. Der Ordner wird nun erstellt."
+    echo -e "$BLUEℹ Info: $NORMAL MySpeed wird unter dem Verzeichnis $INSTALLATION_PATH installiert. Der Ordner wird nun erstellt."
     sleep 2
     mkdir $INSTALLATION_PATH
 fi
 
 cd $INSTALLATION_PATH
 
-echo "$BLUEℹ Info: $NORMAL Die aktuelle MySpeed-Instanz wird heruntergeladen. Einen Moment..."
+echo -e "$BLUEℹ Info: $NORMAL Die aktuelle MySpeed-Instanz wird heruntergeladen. Einen Moment..."
 sleep 2
 wget "$RELEASE_URL"
 
-echo "$BLUEℹ Info: $NORMAL Download abgeschlossen. Entpacken läuft..."
+echo -e "$BLUEℹ Info: $NORMAL Download abgeschlossen. Entpacken läuft..."
 sleep 2
 unzip MySpeed*.zip
 rm MySpeed-*.zip
 
-echo "$BLUEℹ Info: $NORMAL Die notwendigen Abhängigkeiten werden jetzt installiert..."
+echo -e "$BLUEℹ Info: $NORMAL Die notwendigen Abhängigkeiten werden jetzt installiert..."
 sleep 2
 npm install
 
 clear
-echo "$GREEN✓ Installation abgeschlossen: $NORMAL MySpeed wurde unter $INSTALLATION_PATH installiert."
-echo "Die Weboberfläche findest du unter $BLUEhttp://localhost:5216$NORMAL."
+echo -e "$GREEN✓ Installation abgeschlossen: $NORMAL MySpeed wurde unter $INSTALLATION_PATH installiert."
+echo -e "Die Weboberfläche findest du unter $BLUEhttp://localhost:5216$NORMAL."
