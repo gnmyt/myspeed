344e54e4d8b4094c28c2bd3f435b4d575c39f3f3
Mathias Wagner <35641351+gnmyt@users.noreply.github.com>
2022-06-10 12:13:31 +0000

    Moved the "start speedtest"-icon into the header

--- client/src/components/HeaderComponent.js
+++ client/src/components/HeaderComponent.js
@@ -1,22 +1,35 @@
 import "../style/Header.sass";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faGear} from "@fortawesome/free-solid-svg-icons";
+import {faGaugeHigh, faGear} from "@fortawesome/free-solid-svg-icons";
 import DropdownComponent, {toggleDropdown} from "./DropdownComponent";
-import {useState} from "react";
+import {useContext, useState} from "react";
+import { DialogContext } from "../context/DialogContext";
+
 
 function HeaderComponent() {
 
+    const [setDialog] = useContext(DialogContext);
     const [icon, setIcon] = useState(faGear);
 
+    let headers = localStorage.getItem("password") ? {password: localStorage.getItem("password")} : {}
+    headers['content-type'] = 'application/json'
+    
     function switchDropdown() {
         toggleDropdown(setIcon);
+    }
+
+    const startSpeedtest = async () => {
+        setDialog({speedtest: true, promise: fetch("/api/speedtests/run", {headers: headers, method: "POST"})});
     }
 
     return (
         <header>
             <div className="header-main">
                 <h2>Netzwerkanalyse</h2>
-                <FontAwesomeIcon icon={icon} className="settings" onClick={switchDropdown}/>
+                <div className="header-right">
+                    <FontAwesomeIcon icon={faGaugeHigh} className="header-icon" onClick={startSpeedtest} />
+                    <FontAwesomeIcon icon={icon} className="header-icon" onClick={switchDropdown} />
+                </div>
             </div>
             <DropdownComponent/>
         </header>
