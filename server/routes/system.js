bd82df6e6fcecdd2a3fb62cb15a9576fc3f5843f
Mathias Wagner <35641351+gnmyt@users.noreply.github.com>
2022-06-10 13:37:20 +0000

    The version route now shows local & remote

--- server/routes/system.js
+++ server/routes/system.js
@@ -1,8 +1,11 @@
 const app = require('express').Router();
 const version = require('../../package.json').version;
+const remote_url = "https://raw.githubusercontent.com/gnmyt/myspeed/release/package.json";
+const axios = require('axios');
 
-app.get("/version", (req, res) => {
-    res.send("MySpeed Server version " + version);
+
+app.get("/version", async (req, res) => {
+    res.json({local: version, remote: (await axios.get(remote_url)).data.version});
 });
 
 module.exports = app;
