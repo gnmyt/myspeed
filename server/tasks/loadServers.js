9ecf36a167d7ed87611ea1faf6ef919aeafc6314
Mathias Wagner <germannewsmaker@gmail.com>
2022-06-11 02:52:33 +0200

    Created the loadServers task

---
+++ server/tasks/loadServers.js
@@ -0,0 +1,24 @@
+const axios = require('axios');
+const fs = require('fs');
+
+if (!fs.existsSync("data/servers.json")) {
+    let servers = {};
+    try {
+        axios.get("https://www.speedtest.net/api/js/servers?limit=20")
+            .then(res => res.data)
+            .then(data => {
+                data.forEach(row => {
+                    servers[row.id] = row.name + " (" + row.distance + "km)";
+                });
+
+                try {
+                    fs.writeFileSync("data/servers.json", JSON.stringify(servers, null, 4));
+                } catch (e) {
+                    console.error("Could not save servers file")
+                }
+            });
+    } catch (e) {
+        console.error("Could not get servers");
+    }
+
+}
