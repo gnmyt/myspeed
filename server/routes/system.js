630919b6f84de6d6fbc9a9e3a9db48e6be948874
Mathias <germannewsmaker@gmail.com>
2022-08-06 23:51:17 +0200

    Updated the url of the system version route

--- server/routes/system.js
+++ server/routes/system.js
@@ -1,15 +1,15 @@
 const app = require('express').Router();
 const version = require('../../package.json').version;
-const remote_url = "https://raw.githubusercontent.com/gnmyt/myspeed/release/package.json";
+const remote_url = "https://api.github.com/repos/gnmyt/myspeed/releases/latest";
 const axios = require('axios');
 const fs = require("fs");
 
 
 app.get("/version", async (req, res) => {
     try {
-        res.json({local: version, remote: (await axios.get(remote_url)).data.version});
+        res.json({local: version, remote: (await axios.get(remote_url)).data.tag_name});
     } catch (e) {
-        res.status(500).json({message: "An error occurred"});
+        res.json({local: version, remote: "0"});
     }
 });
 
