78a9a5290fe1e97c54f6a55c80e29ad7222e3002
Mathias <germannewsmaker@gmail.com>
2022-08-08 01:27:16 +0200

    Updated the recommendations route

--- server/routes/recommendations.js
+++ server/routes/recommendations.js
@@ -2,9 +2,9 @@ const app = require('express').Router();
 const recommendations = require('../controller/recommendations');
 
 // Gets all config entries
-app.get("/", (req, res) => {
-    let currentRecommendations = recommendations.get();
-    if (currentRecommendations === undefined) return res.status(501).json({message: "There are no recommendations yet"});
+app.get("/", async (req, res) => {
+    let currentRecommendations = await recommendations.get();
+    if (currentRecommendations === null) return res.status(501).json({message: "There are no recommendations yet"});
 
     return res.json(currentRecommendations);
 });
