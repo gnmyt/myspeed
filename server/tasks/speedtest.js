977e05ffed89649e95bdaf9fbbb098ccd310fab2
Mathias <germannewsmaker@gmail.com>
2022-08-08 01:27:51 +0200

    Updated the speedtest task

--- server/tasks/speedtest.js
+++ server/tasks/speedtest.js
@@ -10,7 +10,7 @@ function roundSpeed(bytes, elapsed) {
 }
 
 async function createRecommendations() {
-    let list = tests.list().filter((entry) => !entry.error);
+    let list = (await tests.list()).filter((entry) => !entry.error);
     if (list.length >= 10) {
         let avgNumbers = {ping: 0, down: 0, up: 0};
         for (let i = 0; i < 10; i++) {
@@ -19,7 +19,7 @@ async function createRecommendations() {
             avgNumbers["up"] += list[i].upload;
         }
 
-        recommendations.set(avgNumbers["ping"] / 10, avgNumbers["down"] / 10, avgNumbers["up"] / 10);
+        await recommendations.set(avgNumbers["ping"] / 10, avgNumbers["down"] / 10, avgNumbers["up"] / 10);
     }
 }
 
@@ -32,7 +32,7 @@ module.exports.run = async () => {
     let speedtest = await speedTest({acceptLicense: true, acceptGdpr: true, serverId: serverId});
 
     if (serverId === undefined)
-        config.update("serverId", speedtest.server.id);
+        await config.update("serverId", speedtest.server.id);
 
     return speedtest;
 }
@@ -45,16 +45,16 @@ module.exports.create = async (type = "auto") => {
         let ping = Math.round(test.ping.latency);
         let download = roundSpeed(test.download.bytes, test.download.elapsed);
         let upload = roundSpeed(test.upload.bytes, test.upload.elapsed);
-        let testResult = tests.create(ping, download, upload, Math.round((test.download.elapsed + test.upload.elapsed)/1000), type);
+        let testResult = await tests.create(ping, download, upload, Math.round((test.download.elapsed + test.upload.elapsed)/1000), type);
         console.log(`Test #${testResult} was executed successfully. 🏓 ${ping} ⬇ ${download}️ ⬆ ${upload}️`);
         createRecommendations().then(() => "");
     } catch (e) {
-        let testResult = tests.create(-1, -1, -1, null, type, e.message);
+        let testResult = await tests.create(-1, -1, -1, null, type, e.message);
         console.log(`Test #${testResult} was not executed successfully. Please try reconnecting to the internet or restarting the software.`);
     }
     isRunning = false;
 }
 
-module.exports.removeOld = () => {
-    tests.removeOld();
+module.exports.removeOld = async () => {
+    await tests.removeOld();
 }
