b498b5bffbec52bc23e1f2bf36d0dfa64bd92662
Mathias <germannewsmaker@gmail.com>
2022-08-08 01:22:19 +0200

    Created the Speedtests model

---
+++ server/models/Speedtests.js
@@ -0,0 +1,34 @@
+const Sequelize = require('sequelize');
+const db = require("../config/database");
+
+module.exports = db.define("speedtests", {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    ping: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    download: {
+        type: Sequelize.DOUBLE,
+        allowNull: false
+    },
+    upload: {
+        type: Sequelize.DOUBLE,
+        allowNull: false
+    },
+    error: {
+        type: Sequelize.STRING,
+        allowNull: true
+    },
+    type: {
+        type: Sequelize.STRING,
+        defaultValue: "auto"
+    },
+    time: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0
+    }
+}, {createdAt: "created", updatedAt: false});
