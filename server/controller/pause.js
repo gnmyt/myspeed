c3ebaab58d55ac66ef45dd6bf454248d9bc11a9c
Mathias <germannewsmaker@gmail.com>
2022-08-09 16:50:46 +0200

    Fixed the pause controller

--- server/controller/pause.js
+++ server/controller/pause.js
@@ -8,11 +8,15 @@ module.exports.updateState = function(newState) {
 
 // Update the current state in a specific time
 module.exports.resumeIn = function(time) {
+    if (isNaN(time)) return;
+
     if (updateTimer !== null) 
         clearTimeout(updateTimer);
 
     this.updateState(true);
     updateTimer = setTimeout(() => this.updateState(false), time * 3600000); // time in hours
+
+    return true;
 }
 
 module.exports.currentState = currentState;
