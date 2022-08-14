063749fcabae493a6461dc64692c0fab02fc822d
Mathias <germannewsmaker@gmail.com>
2022-08-14 22:09:29 +0200

    Moved the Home page into the pages directory

---
+++ client/src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import LatestTestComponent from "./components/LatestTest";
+import TestAreaComponent from "./components/TestArea";
+
+const Home = () => (
+    <>
+        <LatestTestComponent/>
+
+        <hr/>
+
+        <TestAreaComponent/>
+    </>
+)
+
+
+export default Home;
