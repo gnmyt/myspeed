1ca686e5f8c215c6fd679a0d1f1db310eaa998de
Mathias <germannewsmaker@gmail.com>
2022-08-15 13:14:58 +0200

    The SpeedtestComponent now supports average speedtests

--- client/src/pages/Home/components/Speedtest/SpeedtestComponent.jsx
+++ client/src/pages/Home/components/Speedtest/SpeedtestComponent.jsx
@@ -22,6 +22,12 @@ const errors = {
     "Could not retrieve or read configuration": "Die Konfigurationsdatei konnte nicht geladen werden",
 }
 
+const tooltips = {
+    custom: "Benutzerdefiniert",
+    average: "Durchschnitt",
+    auto: "Automatisiert",
+}
+
 function SpeedtestComponent(props) {
 
     const [setDialog] = useContext(DialogContext);
@@ -31,9 +37,57 @@ function SpeedtestComponent(props) {
 
     let errorMessage = "Unbekannter Fehler: " + props.error;
 
+    let timeString;
+    if (props.type === "average") {
+        timeString = String(props.time.getDate()).padStart(2, '0') + "." + String(props.time.getMonth() + 1).padStart(2, '0');
+    } else {
+        timeString = String(props.time.getHours()).padStart(2, '0') + ":" + String(props.time.getMinutes()).padStart(2, '0');
+    }
+
     if (props.error) {
         for (let errorsKey in errors)
             if (props.error.includes(errorsKey)) errorMessage = errors[errorsKey];
+    }
+
+    const showErrorDialog = () => {
+        setDialog({
+            title: "Test fehlgeschlagen",
+            description: errorMessage + ". Bitte überprüfe weitestgehend, ob das öfters passiert.",
+            buttonText: "Okay",
+            unsetButton: true,
+            unsetButtonText: "Test löschen",
+            onClear: () => fetch("/api/speedtests/" + props.id, {headers: passwordHeaders, method: "DELETE"})
+                .then(updateTests)
+        });
+    }
+
+    const showInfoDialog = () => {
+        if (props.type === "average") {
+            setDialog({
+                title: "Durchschnittsgeschwindigkeit",
+                buttonText: "Okay",
+                description: <>Anhand von <span className="dialog-value">{props.amount}</span> Tests wurde festgestellt, dass am <span
+                        className="dialog-value">{timeString}</span> durchschnittlich eine Downloadgeschwindigkeit von <span
+                        className="dialog-value">{props.down} Mbit/s</span> und eine Uploadgeschwindigkeit von <span
+                        className="dialog-value">{props.up} Mbit/s</span> bestand. Die Tests haben im Durchschnitt <span
+                        className="dialog-value">{props.duration} Sekunden</span> gedauert.</>
+            });
+        } else {
+            setDialog({
+                title: "Testergebnis",
+                description: <>Dieser Test erreichte eine maximale Downloadgeschwindigkeit von <span
+                    className="dialog-value">{props.down} Mbit/s </span>
+                    und eine maximale Uploadgeschwindigkeit von <span className="dialog-value">{props.up} Mbit/s</span>.
+                    Er wurde <span className="dialog-value">{props.type === "custom"
+                        ? "von dir" : "automatisch"}</span> angelegt und hat <span
+                        className="dialog-value">{props.duration} Sekunden</span> gedauert.</>,
+                buttonText: "Okay",
+                unsetButton: true,
+                unsetButtonText: "Test löschen",
+                onClear: () => fetch("/api/speedtests/" + props.id, {headers: passwordHeaders, method: "DELETE"})
+                    .then(updateTests)
+            });
+        }
     }
 
     return (
@@ -42,29 +96,10 @@ function SpeedtestComponent(props) {
                 <div className="tooltip-element">
                     <FontAwesomeIcon icon={props.error ? faInfo : faClockRotateLeft}
                                      className={"container-icon help-icon icon-" + (props.error ? "error" : "blue")}
-                                     onClick={props.error ? () => setDialog({
-                                         title: "Test fehlgeschlagen",
-                                         description: errorMessage + ". Bitte überprüfe weitestgehend, ob das öfters passiert.",
-                                         buttonText: "Okay",
-                                         unsetButton: true,
-                                         unsetButtonText: "Test löschen",
-                                         onClear: () => fetch("/api/speedtests/"+props.id, {headers: passwordHeaders, method: "DELETE"})
-                                             .then(updateTests)
-                                     }) : () => setDialog({
-                                         title: "Testergebnis",
-                                         description: <>Dieser Test erreichte eine maximale Downloadgeschwindigkeit von <span className="dialog-value">{props.down} Mbit/s </span>
-                                             und eine maximale Uploadgeschwindigkeit von <span className="dialog-value">{props.up} Mbit/s</span>. Er wurde <span className="dialog-value">{props.type === "custom"
-                                                 ? "von dir" : "automatisch"}</span> angelegt und hat <span className="dialog-value">{props.duration} Sekunden</span> gedauert.</>,
-                                         buttonText: "Okay",
-                                         unsetButton: true,
-                                         unsetButtonText: "Test löschen",
-                                         onClear: () => fetch("/api/speedtests/"+props.id, {headers: passwordHeaders, method: "DELETE"})
-                                             .then(updateTests)
-                                     })} />
-                    <span className="tooltip">{props.type === "custom" ? "Benutzerdefiniert" :"Automatisiert"}</span>
+                                     onClick={props.error ? showErrorDialog : showInfoDialog}/>
+                    <span className="tooltip">{tooltips[props.type]}</span>
                 </div>
-
-                <h2 className="date-text">Um {props.time}</h2>
+                <h2 className="date-text">{(props.type === "average" ? "Am " : "Um ") + timeString}</h2>
             </div>
             <div className="speedtest-row">
                 <FontAwesomeIcon icon={props.error ? faClose : faPingPongPaddleBall}
