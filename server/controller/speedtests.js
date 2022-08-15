e1677f6c463cbd952f7d7d517764a093733791c7
Mathias <germannewsmaker@gmail.com>
2022-08-15 13:04:05 +0200

    The speedtest controller now lists speedtests by days

--- server/controller/speedtests.js
+++ server/controller/speedtests.js
@@ -25,6 +25,20 @@ module.exports.list = async (hours = 24) => {
     return dbEntries;
 }
 
+module.exports.listByDays = async (days) => {
+    let dbEntries = (await tests.findAll({order: [["created", "DESC"]]})).filter((entry) => entry.error === null)
+        .filter((entry) => new Date(entry.created) > new Date().getTime() - days * 24 * 3600000);
+
+    let averages = {};
+    dbEntries.forEach((entry) => {
+        const day = new Date(entry.created).toLocaleDateString();
+        if (!averages[day]) averages[day] = [];
+        averages[day].push(entry);
+    });
+
+    return averages;
+}
+
 // Gets the latest speedtest from the database
 module.exports.latest = async () => {
     let speedtest = await tests.findOne({order: [["created", "DESC"]]});
