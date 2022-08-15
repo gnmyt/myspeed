856530fc4b65a4e0247499e098ca41cd64be662b
Mathias <germannewsmaker@gmail.com>
2022-08-15 13:12:19 +0200

    The TestAreaComponent now supports average tests, calculates an id and adds the amount field to the speedtest

--- client/src/pages/Home/components/TestArea/TestAreaComponent.jsx
+++ client/src/pages/Home/components/TestArea/TestAreaComponent.jsx
@@ -14,17 +14,23 @@ function TestArea() {
         <div className="speedtest-area">
             {speedtests.map ? speedtests.map(test => {
                 let date = new Date(Date.parse(test.created));
-                let timeString = String(date.getHours()).padStart(2, '0') + ":" + String(date.getMinutes()).padStart(2, '0');
-                return <Speedtest time={timeString}
+
+                let item = localStorage.getItem("testTime");
+                if ((item === "3" || item === "4") && test.type !== "average") return;
+
+                let id = (test.type === "average") ? date.getDate() + "-" + date.getMonth() : test.id;
+
+                return <Speedtest time={date}
                                   ping={test.ping} pingLevel={getIconBySpeed(test.ping, config.ping, false)}
                                   down={test.download} downLevel={getIconBySpeed(test.download, config.download, true)}
                                   up={test.upload} upLevel={getIconBySpeed(test.upload, config.upload, true)}
                                   error={test.error}
-                                  key={test.id}
+                                  key={id}
                                   url={test.url}
                                   type={test.type}
                                   duration={test.time}
-                                  id={test.id}
+                                  amount={test.amount}
+                                  id={id}
                 />
             }) : ""}
         </div>
