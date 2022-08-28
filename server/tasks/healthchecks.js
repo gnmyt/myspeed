8683f2cdc75875b580fe8ef3de0e0b0773589120
Mathias Wagner <germannewsmaker@gmail.com>
2022-08-28 22:49:13 +0200

    Added the getHealthChecksUrl function into the healthchecks task

--- server/tasks/healthchecks.js
+++ server/tasks/healthchecks.js
@@ -1,6 +1,17 @@
+const config = require('../controller/config');
+
 let currentState = "ping";
 
 // Sets the current state (running = no pings, ping = send pings)
 module.exports.setState = (state = "ping") => {
     currentState = state;
+}
+
+// Get the current health checks url from the config & format it
+const getHealthChecksUrl = async () => {
+    let url = (await config.get("healthChecksUrl")).value.toString();
+    if (url.includes("<uuid>")) return null;
+    if (url.endsWith("/")) url = url.substring(0, url.length - 1);
+
+    return url;
 }
