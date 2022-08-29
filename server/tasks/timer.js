434d00f7b6bdadd0ef5fc21adfc4927c0c5258ac
Mathias Wagner <germannewsmaker@gmail.com>
2022-08-29 12:43:45 +0200

    Removed the get rule function from the timer task

--- server/tasks/timer.js
+++ server/tasks/timer.js
@@ -7,30 +7,6 @@ module.exports.startTimer = (timeLevel) => {
     job = schedule.scheduleJob(getRuleFromLevel(timeLevel), () => this.runTask());
 }
 
-const getRuleFromLevel = (level) => {
-    const rule = new schedule.RecurrenceRule();
-
-    switch (level) {
-        case "1":
-            rule.second = 0;
-            break;
-        case "2":
-            rule.minute = [0, 30]
-            break;
-        case "4":
-            rule.hour = [0, 3, 6, 9, 12, 15, 18, 21];
-            break;
-        case "5":
-            rule.hour = [0, 6, 12, 18];
-            break;
-        default:
-            rule.minute = 0;
-            break;
-    }
-
-    return rule;
-}
-
 module.exports.runTask = async () => {
     if (pauseController.currentState) {
         console.warn("Speedtests currently paused. Trying again later...");
