1ee901e40671356accc4da706aa6454c5d82fbdd
Mathias Wagner <germannewsmaker@gmail.com>
2022-08-30 17:28:45 +0200

    Moved the options of the DropdownComponent.jsx into a separate file

---
+++ client/src/common/components/Dropdown/utils/options.js
@@ -0,0 +1,19 @@
+export const timeOptions = {
+    1: "24 Stunden (Standard)",
+    2: "2 Tage (Insgesamt)",
+    3: "7 Tage (Durchschnitt)",
+    4: "30 Tage (Durchschnitt)"
+};
+
+export const selectOptions = {
+    "* * * * *": "Durchgehend (jede Minute)",
+    "0,30 * * * *": "Sehr häufig (alle 30 Minuten)",
+    "0 * * * *": "Standard (jede Stunde)",
+    "0 0,3,6,9,12,15,18,21 * * *": "Selten (alle 3 Stunden)",
+    "0 0,6,12,18 * * *": "Sehr selten (alle 6 Stunden)"
+}
+
+export const exportOptions = {
+    json: "JSON-Datei",
+    csv: "CSV-Datei"
+}
