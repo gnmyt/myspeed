5c7334c234a4b7634ef09097d68152685915a93b
Mathias Wagner <germannewsmaker@gmail.com>
2022-08-30 18:05:20 +0200

    Moved SpeedtestComponent.jsx errors -> errors.js

---
+++ client/src/pages/Home/components/Speedtest/utils/errors.js
@@ -0,0 +1,10 @@
+export const errors = {
+    "Network unreachable": "Die Internetverbindung scheint unterbrochen gewesen zu sein",
+    "Timeout occurred in connect": "Der Test hat zu lange gedauert und wurde abgebrochen",
+    "permission denied": "MySpeed hat keine Berechtigung, diesen Test zu starten",
+    "Resource temporarily unavailable": "Der Test konnte nicht durchgeführt werden, da die Ressource vorübergehend nicht verfügbar ist",
+    "No route to host": "Der Test konnte nicht durchgeführt werden, da keine Route zum Host existiert",
+    "Connection refused": "Der Test konnte nicht durchgeführt werden, da die Verbindung abgelehnt wurde",
+    "timed out": "Der Test konnte nicht durchgeführt werden, da die Verbindung zu lange gedauert hat",
+    "Could not retrieve or read configuration": "Die Konfigurationsdatei konnte nicht geladen werden",
+}
