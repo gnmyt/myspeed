fa76b561c865d1d2f7e6a35678cdba9b64e738c2
Mathias Wagner <germannewsmaker@gmail.com>
2022-10-16 14:34:04 +0200

    Created the licensing dialog & created all variables for the ookla urls

--- client/src/common/contexts/Config/dialog.jsx
+++ client/src/common/contexts/Config/dialog.jsx
@@ -1,3 +1,10 @@
+import {patchRequest} from "@/common/utils/RequestUtil";
+
+const OOKLA_ABOUT_URL = "https://www.speedtest.net/about";
+const OOKLA_TERMS_URL = OOKLA_ABOUT_URL + "/terms";
+const OOKLA_EULA_URL = OOKLA_ABOUT_URL + "/eula";
+const OOKLA_PRIVACY_URL = OOKLA_ABOUT_URL + "/privacy";
+
 export const passwordRequiredDialog = {
     title: "Passwort erforderlich",
     placeholder: "Dein Passwort",
@@ -9,4 +16,15 @@ export const passwordRequiredDialog = {
         localStorage.setItem("password", value);
         window.location.reload();
     }
+}
+
+export const acceptDialog = {
+    title: "Nutzungsbedingungen akzeptieren",
+    description: <>Mit dem Klick auf <span className="dialog-value">Akzeptieren</span> bestätigst du, dass du die <a
+        href={OOKLA_EULA_URL} target="_blank">EULA</a>, <a href={OOKLA_PRIVACY_URL}
+                                                           target="_blank">Datenschutzerklärung</a> und <a
+        href={OOKLA_TERMS_URL} target="_blank">Nutzungsbedingungen</a> von Ookla gelesen hast und diesen zustimmst.</>,
+    buttonText: "Akzeptieren",
+    disableCloseButton: true,
+    onSuccess: () => patchRequest("/config/acceptOoklaLicense", {value: true})
 }
