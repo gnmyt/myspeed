d34ffc3954f0747711223f92bc8bc81dd310966f
Mathias Wagner <germannewsmaker@gmail.com>
2022-10-22 23:28:12 +0200

    Implemented the i18n api into the index.js

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,24 @@
 import React from 'react';
 import ReactDOM from "react-dom/client";
+import i18n from "i18next";
+import {initReactI18next} from "react-i18next";
+import LanguageDetector from 'i18next-browser-languagedetector';
+import HttpApi from 'i18next-http-backend';
+
 import App from './App';
+
+
+i18n.use(initReactI18next).use(LanguageDetector).use(HttpApi).init({
+    supportedLngs: ['en', 'de'],
+    fallbackLng: 'en',
+    backend: {
+      loadPath: '/locales/{{lng}}.json'
+    },
+    detection: {
+        order: ['localStorage', 'htmlTag'],
+        lookupLocalStorage: 'language'
+    }
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
