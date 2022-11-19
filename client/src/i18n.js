27fd89b2891cbdedd921ca5e83740334f7d4d341
Mathias Wagner <germannewsmaker@gmail.com>
2022-11-19 21:56:29 +0100

    Removed the "await" from the i18n initialization

--- client/src/i18n.js
+++ client/src/i18n.js
@@ -3,7 +3,7 @@ import {initReactI18next} from "react-i18next";
 import LanguageDetector from 'i18next-browser-languagedetector';
 import HttpApi from 'i18next-http-backend';
 
-await i18n.use(initReactI18next).use(LanguageDetector).use(HttpApi).init({
+i18n.use(initReactI18next).use(LanguageDetector).use(HttpApi).init({
     supportedLngs: ['en', 'de'],
     fallbackLng: 'en',
     backend: {
