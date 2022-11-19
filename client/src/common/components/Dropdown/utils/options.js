0f8ad560e970d355a6357df18b5bb7ac0db1f33c
Mathias Wagner <germannewsmaker@gmail.com>
2022-11-19 18:02:00 +0100

    Implemented all translations into the options.jsx

--- client/src/common/components/Dropdown/utils/options.js
+++ client/src/common/components/Dropdown/utils/options.js
@@ -1,24 +1,26 @@
-export const timeOptions = {
-    1: "24 Stunden (Standard)",
-    2: "2 Tage (Insgesamt)",
-    3: "7 Tage (Durchschnitt)",
-    4: "30 Tage (Durchschnitt)"
-};
+import {t} from "i18next";
 
-export const selectOptions = {
-    "* * * * *": "Durchgehend (jede Minute)",
-    "0,30 * * * *": "Sehr häufig (alle 30 Minuten)",
-    "0 * * * *": "Standard (jede Stunde)",
-    "0 0,3,6,9,12,15,18,21 * * *": "Selten (alle 3 Stunden)",
-    "0 0,6,12,18 * * *": "Sehr selten (alle 6 Stunden)"
-}
+export const timeOptions = () => ({
+    1: t("options.time.24hours"),
+    2: t("options.time.2days"),
+    3: t("options.time.7days"),
+    4: t("options.time.30days")
+});
+
+export const selectOptions = () => ({
+    "* * * * *": t("options.cron.continuous"),
+    "0,30 * * * *": t("options.cron.frequent"),
+    "0 * * * *": t("options.cron.default"),
+    "0 0,3,6,9,12,15,18,21 * * *": t("options.cron.rare"),
+    "0 0,6,12,18 * * *": t("options.cron.really_rare")
+});
 
 export const languageOptions = {
     "en": "English",
     "de": "Deutsch"
 }
 
-export const exportOptions = {
-    json: "JSON-Datei",
-    csv: "CSV-Datei"
-}
+export const exportOptions = () => ({
+    json: t("options.export.json"),
+    csv: t("options.export.csv")
+});
