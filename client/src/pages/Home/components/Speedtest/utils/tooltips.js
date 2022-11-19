16dc2f37d1941edd9792015744b33ca9f8185516
Mathias Wagner <germannewsmaker@gmail.com>
2022-11-19 20:18:30 +0100

    Implemented all translations into the tooltips.js

--- client/src/pages/Home/components/Speedtest/utils/tooltips.js
+++ client/src/pages/Home/components/Speedtest/utils/tooltips.js
@@ -1,5 +1,7 @@
-export const tooltips = {
-    custom: "Benutzerdefiniert",
-    average: "Durchschnitt",
-    auto: "Automatisiert",
-}
+import {t} from "i18next";
+
+export const tooltips = () => ({
+    custom: t("test.tooltips.custom"),
+    average: t("test.tooltips.average"),
+    auto: t("test.tooltips.auto"),
+});
