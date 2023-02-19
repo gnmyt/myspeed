867107aa13cee499adf3a5cec2b156677afe3435
Mathias Wagner <germannewsmaker@gmail.com>
2023-02-19 16:35:32 +0100

    Removed the i18n instance from the index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from "react-dom/client";
 import App from './App';
-import i18n from './i18n';
 
 export const PROJECT_URL = "https://github.com/gnmyt/myspeed";
 export const PROJECT_WIKI = "https://myspeed.gnmyt.dev";
