52418e51c13d433244acf8564d01dd74829f99ac
Mathias Wagner <germannewsmaker@gmail.com>
2023-04-04 01:11:58 +0200

    Created the Nodes page

--- client/src/pages/Nodes/Nodes.jsx
+++ client/src/pages/Nodes/Nodes.jsx
@@ -1,3 +1,29 @@
-export const Nodes = () => {
+import "./styles.sass";
+import NodeHeader from "@/pages/Nodes/components/NodeHeader";
+import NodeContainer from "@/pages/Nodes/components/NodeContainer";
+import {useContext, useState} from "react";
+import {NodeContext} from "@/common/contexts/Node";
+import {t} from "i18next";
+import CreateNodeDialog from "@/pages/Nodes/components/CreateNodeDialog";
 
+export const Nodes = (props) => {
+    const [nodes] = useContext(NodeContext);
+    const [createDialogOpen, setCreateDialogOpen] = useState(false);
+
+    return (
+        <div className="node-page">
+            {createDialogOpen && <CreateNodeDialog onClose={() => setCreateDialogOpen(false)}/>}
+            <NodeHeader/>
+            <div className="node-area">
+                <NodeContainer name={"Dieser Server"} url={location.href} currentNode={true}
+                               setShowNodePage={props.setShowNodePage} id={0}/>
+
+                {nodes.map(node => <NodeContainer {...node} key={node.id} setShowNodePage={props.setShowNodePage} />)}
+
+                <div className="node-add" onClick={() => setCreateDialogOpen(true)}>
+                    <h1>{t("nodes.add")}</h1>
+                </div>
+            </div>
+        </div>
+    )
 }
