74708d01241f77b6e5aba9f358878db4a231b010
Mathias Wagner <germannewsmaker@gmail.com>
2023-04-05 14:51:41 +0200

    Fixed a bug in the NodeContext.jsx

--- client/src/common/contexts/Node/NodeContext.jsx
+++ client/src/common/contexts/Node/NodeContext.jsx
@@ -24,7 +24,7 @@ export const NodeProvider = (props) => {
         setCurrentNode(parseInt(node));
     }
 
-    const findNode = (nodeId) => nodes.find(node => node.id === nodeId);
+    const findNode = (nodeId) => nodes?.find(node => node.id === nodeId);
 
     return (
         <NodeContext.Provider value={[nodes, updateNodes, currentNode, updateCurrentNode, findNode]}>
