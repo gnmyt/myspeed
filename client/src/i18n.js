638f6ab25aac205199ce5fc9804280dbb89f90e8
Mathias Wagner <germannewsmaker@gmail.com>
2023-05-07 20:55:42 +0200

    MySpeed now sets the default language depending on the browser navigator

--- client/src/i18n.js
+++ client/src/i18n.js
@@ -3,6 +3,9 @@ import {initReactI18next} from "react-i18next";
 import LanguageDetector from 'i18next-browser-languagedetector';
 import HttpApi from 'i18next-http-backend';
 
+if (localStorage.getItem('language') === null)
+    localStorage.setItem('language', navigator.language.split('-')[0]);
+
 i18n.use(initReactI18next).use(LanguageDetector).use(HttpApi).init({
     supportedLngs: ['en', 'de'],
     fallbackLng: 'en',
