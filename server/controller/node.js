ae8f27b4daa53d7d4803d1ef23b0a8d1a5fb607e
Mathias Wagner <germannewsmaker@gmail.com>
2023-11-30 12:32:00 +0100

    Updated the node.js controller

--- server/controller/node.js
+++ server/controller/node.js
@@ -1,37 +1,20 @@
 const axios = require('axios');
 const nodes = require('../models/Node');
 
-// Gets all node entries
-module.exports.list = async () => {
-    return await nodes.findAll().then((result) => result.map((node) => ({...node, password: node.password !== null})));
-}
+module.exports.listAll = async () => await nodes.findAll()
+    .then((result) => result.map((node) => ({...node, password: node.password !== null})));
 
-// Create a new node entry
-module.exports.create = async (name, url, password) => {
-    return await nodes.create({name: name, url: url, password: password});
-}
+module.exports.create = async (name, url, password) => await nodes.create({name: name, url: url, password: password});
 
-// Delete a node entry
-module.exports.delete = async (nodeId) => {
-    return await nodes.destroy({where: {id: nodeId}});
-}
+module.exports.delete = async (nodeId) => await nodes.destroy({where: {id: nodeId}});
 
-// Get a specific node entry
-module.exports.get = async (nodeId) => {
-    return await nodes.findOne({where: {id: nodeId}});
-}
+module.exports.getOne = async (nodeId) => await nodes.findOne({where: {id: nodeId}});
 
-// Update the name of the node entry
-module.exports.updateName = async (nodeId, name) => {
-    return await nodes.update({name: name}, {where: {id: nodeId}});
-}
+module.exports.updateName = async (nodeId, name) => await nodes.update({name: name}, {where: {id: nodeId}});
 
-// Update the password of the node entry
-module.exports.updatePassword = async (nodeId, password) => {
-    return await nodes.update({password: password}, {where: {id: nodeId}});
-}
+module.exports.updatePassword = async (nodeId, password) => await nodes.update({password: password}, {where: {id: nodeId}});
 
-module.exports.checkNode = async (url, password) => {
+module.exports.checkStatus = async (url, password) => {
     if (password === "none") password = undefined;
     const api = await axios.get(url + "/api/config", {headers: {password: password}}).catch(() => {
         return "INVALID_URL";
