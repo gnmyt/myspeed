2c1d5ae2a517a1cdbacb2c4ac16755452a0c47f9
Mathias Wagner <germannewsmaker@gmail.com>
2024-03-09 20:04:01 +0100

    Created the videos.jsx source

---
+++ web/src/pages/Tutorials/sources/videos.jsx
@@ -0,0 +1,55 @@
+/**
+ * @file This file contains the data for the videos section of the Tutorials page
+ */
+
+// Creators
+import BigBearTechChannel from "./channels/bigbeartechworld.png";
+import RetroMikeTechChannel from "./channels/retromiketech.png";
+import Pavl21Channel from "./channels/pavl21.png";
+
+// Thumbnails
+import Thumb_Iic14oUCCVo from "./thumbs/Iic14oUCCVo.webp";
+import Thumb_ZIIa6yF_Tvo from "./thumbs/ZIIa6yF-Tvo.webp";
+import Thumb_SM3RJRktwIk from "./thumbs/SM3RJRktwIk.webp";
+import Thumb_7roj87Fytz0 from "./thumbs/7roj87Fytz0.webp";
+import Thumb_MFbeWdKesTE from "./thumbs/MFbeWdKesTE.webp";
+import Thumb_7108075382452079878 from "./thumbs/7108075382452079878.webp";
+
+export default [
+    {
+        title: "How to install MySpeed on Dockge",
+        link: "https://www.youtube.com/watch?v=Iic14oUCCVo",
+        thumb: Thumb_Iic14oUCCVo,
+        creator: BigBearTechChannel
+    },
+    {
+        title: "How to install MySpeed on Cosmos Cloud",
+        link: "https://www.youtube.com/watch?v=ZIIa6yF-Tvo",
+        thumb: Thumb_ZIIa6yF_Tvo,
+        creator: BigBearTechChannel
+    },
+    {
+        title: "How to install MySpeed on Portainer",
+        link: "https://www.youtube.com/watch?v=SM3RJRktwIk",
+        thumb: Thumb_SM3RJRktwIk,
+        creator: BigBearTechChannel
+    },
+    {
+        title: "How to install Myspeed on CasaOS using BigBearCasaOS",
+        link: "https://www.youtube.com/watch?v=7roj87Fytz0",
+        thumb: Thumb_7roj87Fytz0,
+        creator: BigBearTechChannel
+    },
+    {
+        title: "Monitor Internet Bandwidth Speeds for Crypto Nodes | MySpeed",
+        link: "https://www.youtube.com/watch?v=MFbeWdKesTE",
+        thumb: Thumb_MFbeWdKesTE,
+        creator: RetroMikeTechChannel
+    },
+    {
+        title: "MySpeed Automatisierte Speedtests",
+        link: "https://www.tiktok.com/@paul_schwarz_/video/7108075382452079878",
+        thumb: Thumb_7108075382452079878,
+        creator: Pavl21Channel
+    }
+]
