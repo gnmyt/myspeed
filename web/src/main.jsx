cacc585f3af7facf2e9636179986e573d4500167
Mathias Wagner <germannewsmaker@gmail.com>
2024-03-09 18:09:11 +0100

    Added the DOCUMENTATION_BASE string to the main.jsx

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -8,6 +8,7 @@ import NotFound from "@/pages/NotFound";
 import Home from "@/pages/Home";
 import Imprint from "@/pages/Imprint";
 import Privacy from "@/pages/Privacy";
+import Install from "@/pages/Install";
 
 const router = createBrowserRouter([
     {
@@ -16,13 +17,15 @@ const router = createBrowserRouter([
         errorElement: <NotFound />,
         children: [
             {path: "/", element: <Home />},
-            {path: "/install", element: <h2>Install</h2>},
+            {path: "/install", element: <Install />},
             {path: "/tutorials", element: <h2>Tutorials</h2>},
             {path: "/imprint", element: <Imprint />},
             {path: "/privacy", element: <Privacy />},
         ]
     }
 ]);
+
+export const DOCUMENTATION_BASE = "https://myspeed.gnmyt.dev";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
