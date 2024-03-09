f0f7bc0798f2d1d6946397406897aad3eeed8549
Mathias Wagner <germannewsmaker@gmail.com>
2024-03-09 14:40:04 +0100

    Removed zooming in the Root.jsx

--- web/src/common/layouts/Root/Root.jsx
+++ web/src/common/layouts/Root/Root.jsx
@@ -1,8 +1,28 @@
 import {Outlet} from "react-router-dom";
 import Navigation from "@/common/components/Navigation";
 import "./styles.sass";
+import {useEffect} from "react";
 
 export const Root = () => {
+
+    useEffect(() => {
+        const handleKeydown = (event) => {
+            if (event.ctrlKey && (event.key === "+" || event.key === "-")) event.preventDefault();
+        }
+
+        const handleWheel = (event) => {
+            if (event.ctrlKey) event.preventDefault();
+        }
+
+        document.addEventListener("keydown", handleKeydown);
+        document.addEventListener("wheel", handleWheel, {passive: false});
+
+        return () => {
+            document.removeEventListener("keydown", handleKeydown);
+            document.removeEventListener("wheel", handleWheel);
+        }
+    }, []);
+
     return (
         <>
             <Navigation />
