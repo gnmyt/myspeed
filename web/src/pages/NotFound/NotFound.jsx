8c15b506023fae80e32441254f3cf3f91fc6dffb
Mathias Wagner <germannewsmaker@gmail.com>
2024-03-09 14:51:26 +0100

    Created the NotFound.jsx page

--- web/src/pages/NotFound/NotFound.jsx
+++ web/src/pages/NotFound/NotFound.jsx
@@ -1,5 +1,29 @@
+import {useEffect} from "react";
+import "./styles.sass";
+import NotFoundImage from "@/common/assets/not_found.svg";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faQuestionCircle} from "@fortawesome/free-solid-svg-icons";
+
 export const NotFound = () => {
-  return (
-      <h2>Page Not Found</h2>
-  );
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            window.location.href = "/";
+        }, 2000);
+
+        return () => clearTimeout(timeout);
+    }, []);
+
+    return (
+        <div className="page-404">
+            <div className="info-area">
+                <div className="logo-container">
+                    <FontAwesomeIcon icon={faQuestionCircle} size="3x" />
+                    <h1>Page not found</h1>
+                </div>
+                <p>You will be redirected to the home page in a few seconds.</p>
+            </div>
+            <img src={NotFoundImage} alt="Interface" />
+        </div>
+    );
 }
