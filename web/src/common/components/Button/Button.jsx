48c4113be7003b9545aa373a8651a32299471afc
Mathias Wagner <germannewsmaker@gmail.com>
2024-03-09 18:08:48 +0100

    Added the Button disabled property

--- web/src/common/components/Button/Button.jsx
+++ web/src/common/components/Button/Button.jsx
@@ -1,10 +1,10 @@
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import "./styles.sass";
 
-export const Button = ({color, text, icon, onClick}) => {
+export const Button = ({color, text, icon, onClick, disabled}) => {
     return (
-        <button className={`btn btn-color-${color}`} onClick={onClick}>
-            <FontAwesomeIcon icon={icon} />
+        <button className={`btn btn-color-${color}`} onClick={onClick} disabled={disabled}>
+            <FontAwesomeIcon icon={icon}/>
             <h1>{text}</h1>
         </button>
     );
