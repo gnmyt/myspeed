10bdde761e32f9dcaa964e6718ae05d269c75415
Mathias Wagner <germannewsmaker@gmail.com>
2024-03-09 22:33:50 +0100

    Translated the install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-# Colors for better overview
 GREEN='\033[0;32m'
 BLUE='\033[1;34m'
 YELLOW='\033[1;33m'
@@ -17,40 +16,37 @@ while getopts "d:" o > /dev/null 2>&1; do
     esac
 done
 
-# Root check
 if [ $EUID -ne 0 ]; then
   echo -e "$RED-$NORMAL-$RED-$NORMAL-$RED-$NORMAL-$RED-$NORMAL-$RED-$NORMAL-$RED-$NORMAL-$RED-$NORMAL-$RED-$NORMAL-$RED-$NORMAL-"
-  echo -e "$RED✗ ABGEBROCHEN"
-  echo -e "$NORMAL Die Installation läuft derzeit über einen Benutzer ohne Root-Rechte. Dies ist allerdings erforderlich. Melde dich mit einem Root Account an, um fortzufahren."
+  echo -e "$RED✗ ABORTED"
+  echo -e "$NORMAL The installation is currently running via a user without root privileges. However, this is required. Please log in with a Root Account to continue."
   echo -e "$RED-$NORMAL-$RED-$NORMAL-$RED-$NORMAL-$RED-$NORMAL-$RED-$NORMAL-$RED-$NORMAL-$RED-$NORMAL-$RED-$NORMAL-$RED-$NORMAL-"
   exit
 fi
 
-echo -e "$GREEN ---------$BLUE Automatische Installation$GREEN ---------"
-echo -e "$BLUE MySpeed$YELLOW wird nun installiert."
+echo -e "$GREEN ---------$BLUE Automatic Installation$GREEN ---------"
+echo -e "$BLUE MySpeed$YELLOW is now being installed."
 if [ "$1" == "--beta" ]; then
   echo -e "$YELLOW Version:$BLUE MySpeed$PURPLE Beta"
 else
   echo -e "$YELLOW Version:$BLUE MySpeed Release"
 fi
-echo -e "$YELLOW Ort:$BLUE $INSTALLATION_PATH"
-echo -e "$YELLOW Es wird die Speedtest API von Ookla verwendet."
-echo -e "$YELLOW Wenn du damit$RED nicht$YELLOW einverstanden bist,"
-echo -e "$YELLOW kannst du die Installation mit$RED STRG + C$YELLOW abbrechen. "
-echo -e "$GREEN Die Installation beginnt in 10 Sekunden..."
+echo -e "$YELLOW Location:$BLUE $INSTALLATION_PATH"
+echo -e "$YELLOW Speedtest API by Ookla will be used."
+echo -e "$YELLOW If you$RED disagree$YELLOW with this, you can cancel the installation by pressing$RED CTRL + C$YELLOW. "
+echo -e "$GREEN Installation will start in 10 seconds..."
 echo -e "$GREEN ----------------------------------------------"
 sleep 10
 clear
 
-# Check if installed
 if [ -d "$INSTALLATION_PATH" ]; then
     clear
     echo -e "$YELLOW-$NORMAL-$YELLOW-$NORMAL-$YELLOW-$NORMAL-$YELLOW-$NORMAL-$YELLOW-$NORMAL-$YELLOW-$NORMAL-$YELLOW-$NORMAL-$YELLOW-$NORMAL-$YELLOW-$NORMAL-"
     echo -e ""
-    echo -e "$YELLOW⚠ WARNUNG"
-    echo -e "$NORMAL MySpeed ist bereits auf diesem System installiert."
+    echo -e "$YELLOW⚠ WARNING"
+    echo -e "$NORMAL MySpeed is already installed on this system."
     echo -e ""
-    echo -e "$GREENℹ Info:$NORMAL Neuestes Update wird installiert..."
+    echo -e "$GREENℹ Info:$NORMAL Latest update will be installed..."
     echo -e ""
     echo -e "$YELLOW-$NORMAL-$YELLOW-$NORMAL-$YELLOW-$NORMAL-$YELLOW-$NORMAL-$YELLOW-$NORMAL-$YELLOW-$NORMAL-$YELLOW-$NORMAL-$YELLOW-$NORMAL-$YELLOW-$NORMAL-"
     sleep 5
@@ -58,39 +54,38 @@ fi
 
 if command -v systemctl &> /dev/null && systemctl --all --type service | grep -n "myspeed.service"; then
   clear
-  echo -e "$YELLOWℹ MySpeed Dienst wird gestoppt..."
+  echo -e "$YELLOWℹ MySpeed Service is being stopped..."
   systemctl stop myspeed
 fi
 
 
-# Update all packages
 clear
 echo -e ""
 echo -e "$BLUE-$NORMAL-$BLUE-$NORMAL-$BLUE-$NORMAL-$BLUE-$NORMAL-$BLUE-$NORMAL-$BLUE-$NORMAL-$BLUE-$NORMAL-$BLUE-$NORMAL-$BLUE-$NORMAL-"
 echo -e ""
-echo -e "$BLUE🔎 STATUSMELDUNG"
-echo -e "$NORMAL Update-Suche für Linux-System gestartet..."
+echo -e "$BLUE🔎 STATUS MESSAGE"
+echo -e "$NORMAL Searching for updates for Linux system..."
 echo -e ""
 echo -e "$BLUE-$NORMAL-$BLUE-$NORMAL-$BLUE-$NORMAL-$BLUE-$NORMAL-$BLUE-$NORMAL-$BLUE-$NORMAL-$BLUE-$NORMAL-$BLUE-$NORMAL-$BLUE-$NORMAL-"
 echo -e ""
 apt-get update -y
 
 clear
-echo -e "$GREENℹ Info:$NORMAL Die Installation wird jetzt vorbereitet. Das kann einen Augenblick dauern..."
+echo -e "$GREENℹ Info:$NORMAL Installation is now being prepared. This may take a moment..."
 sleep 5
 
 function check() {
   clear
   echo -e "$BLUE-$NORMAL-$BLUE-$NORMAL-$BLUE-$NORMAL-$BLUE-$NORMAL-$BLUE-$NORMAL-$BLUE-$NORMAL-$BLUE-$NORMAL-$BLUE-$NORMAL-$BLUE-$NORMAL-"
-  echo -e "$BLUE🔎 STATUSMELDUNG"
-  echo -e "$NORMAL Es wird überprüft, ob $1 vorhanden ist..."
+  echo -e "$BLUE🔎 STATUS MESSAGE"
+  echo -e "$NORMAL Checking if $1 is present..."
   echo -e "$BLUE-$NORMAL-$BLUE-$NORMAL-$BLUE-$NORMAL-$BLUE-$NORMAL-$BLUE-$NORMAL-$BLUE-$NORMAL-$BLUE-$NORMAL-$BLUE-$NORMAL-$BLUE-$NORMAL-"
   echo -e ""
   if ! command -v "$1" &> /dev/null
   then
-      echo -e "$YELLOWℹ \"$1\" ist nicht installiert.$NORMAL Die Installation wird gestartet..."
+      echo -e "$YELLOWℹ \"$1\" is not installed.$NORMAL Installation will proceed..."
       sleep 2
-      echo -e "$PURPLEℹ Wird installiert..."
+      echo -e "$PURPLEℹ Installing..."
       apt-get install "$1" -y
   fi
 }
@@ -99,19 +94,18 @@ check "wget"
 check "unzip"
 check "curl"
 
-# Check for node
 clear
-echo -e "$BLUE🔎 STATUSMELDUNG"
-echo -e "$NORMAL Es wird überprüft, ob node vorhanden ist..."
+echo -e "$BLUE🔎 STATUS MESSAGE"
+echo -e "$NORMAL Checking if node is present..."
 if ! command -v node &> /dev/null
 then
-    echo -e "$YELLOWℹ \"node\" ist nicht installiert.$NORMAL Die Installation wird gestartet..."
+    echo -e "$YELLOWℹ \"node\" is not installed.$NORMAL Installation will proceed..."
     sleep 2
     clear
-    echo -e "$PURPLEℹ Wird heruntergeladen...$NORMAL"
-    curl -sSL https://deb.nodesource.com/setup_16.x | bash
+    echo -e "$PURPLEℹ Downloading...$NORMAL"
+    curl -sSL https://deb.nodesource.com/setup_20.x | bash
     clear
-    echo -e "$PURPLEℹ Wird installiert...$NORMAL"
+    echo -e "$PURPLEℹ Installing...$NORMAL"
     apt-get install nodejs -y
 fi
 
@@ -124,14 +118,14 @@ else
 fi
 
 
-echo -e "$GREEN✓ Vorbereitung abgeschlossen:$NORMAL Die Installation von MySpeed wird jetzt gestartet..."
+echo -e "$GREEN✓ Preparation completed:$NORMAL Installation of MySpeed will now commence..."
 sleep 3
 
 clear
 if [ ! -d "$INSTALLATION_PATH" ]
 then
     clear
-    echo -e "$BLUEℹ Info: $NORMAL MySpeed wird unter dem Verzeichnis $INSTALLATION_PATH installiert. Der Ordner wird nun erstellt."
+    echo -e "$BLUEℹ Info: $NORMAL MySpeed will be installed under directory $INSTALLATION_PATH. Creating the folder now."
     sleep 2
     mkdir "$INSTALLATION_PATH"
 fi
@@ -139,11 +133,11 @@ fi
 cd "$INSTALLATION_PATH"
 
 clear
-echo -e "$BLUEℹ Info: $NORMAL Die aktuelle MySpeed-Instanz wird heruntergeladen. Einen Moment..."
+echo -e "$BLUEℹ Info: $NORMAL The current MySpeed instance is being downloaded. Please wait..."
 sleep 2
 wget "$RELEASE_URL"
 
-echo -e "$BLUEℹ Info: $NORMAL Download abgeschlossen. Entpacken läuft..."
+echo -e "$BLUEℹ Info: $NORMAL Download completed. Unpacking..."
 sleep 2
 if [ "$1" == "--beta" ]; then
   unzip -qo development.zip
@@ -158,25 +152,26 @@ fi
 
 
 clear
-echo -e "$BLUEℹ Info: $NORMAL Die notwendigen Abhängigkeiten werden jetzt installiert..."
+echo -e "$BLUEℹ Info: $NORMAL Necessary dependencies are being installed..."
 sleep 2
 rm -rf "$INSTALLATION_PATH/node_modules"
 npm install --force
 
 if [ "$1" == "--beta" ]; then
   clear
-  echo -e "$BLUEℹ Info: $NORMAL Die Weboberfläche wird kompiliert..."
+  echo -e "$BLUEℹ Info: $NORMAL Web interface is being compiled..."
   sleep 2
-  cd client && npm install --force
+  cd client && npm install --
+
+force
   cd .. && npm run build
   cp -r client/build .
   rm -rf client/build
 fi
 
-# Install as system service
 clear
-echo -e "$BLUE🔎 STATUSMELDUNG"
-echo -e "$NORMAL Registriere MySpeed als Hintergrunddienst..."
+echo -e "$BLUE🔎 STATUS MESSAGE"
+echo -e "$NORMAL Registering MySpeed as a background service..."
 echo -e ""
 echo -e ""
 sleep 2
@@ -198,10 +193,10 @@ if command -v systemctl &> /dev/null && ! systemctl --all --type service | grep 
   WantedBy=multi-user.target
 EOF
   systemctl daemon-reload
-  echo -e "$NORMALℹ MySpeed wird im Autostart hinzugefügt..."
+  echo -e "$NORMALℹ MySpeed will be added to autostart..."
   sleep 1
   systemctl enable myspeed
-  echo -e "$NORMALℹ MySpeed Dienst wird gestartet..."
+  echo -e "$NORMALℹ MySpeed service is starting..."
   sleep 1
   systemctl start myspeed
 fi
@@ -209,21 +204,20 @@ fi
 clear
 
 if ! command -v systemctl &> /dev/null; then
-    echo -e "$YELLOW⚠ Warnung: $NORMAL Dein Linux-System bietet derzeit nicht die Möglichkeit, MySpeed im Hintergrund zu starten. Hierfür wird \"systemd\" benötigt."
-    echo -e "$BLUEℹ Info: $NORMAL Du kannst, wenn du \"systemd\" installiert hast, die Installation erneut starten. Es wird dann automatisch eingestellt."
+    echo -e "$YELLOW⚠ Warning: $NORMAL Your Linux system currently does not support starting MySpeed in the background. \"systemd\" is required for this purpose."
+    echo -e "$BLUEℹ Info: $NORMAL If you have installed \"systemd\", you can restart the installation. It will be set up automatically."
     sleep 5
 else
-  echo -e "$GREENℹ MySpeed wird neu gestartet..."
+  echo -e "$GREENℹ MySpeed is being restarted..."
   sleep 2
   systemctl restart myspeed
 fi
 
 clear
-echo -e "$GREEN-$NORMAL-$GREEN-$NORMAL-$GREEN-$NORMAL-$GREEN-$NORMAL-$GREEN-$NORMAL-$GREEN-$NORMAL-$GREEN-$NORMAL-$GREEN-$NORMAL-$GREEN-$NORMAL-" #multicolor
-echo -e "$GREEN✓ Installation abgeschlossen: $NORMAL MySpeed wurde unter $INSTALLATION_PATH installiert."
-echo -e "Die Weboberfläche findest du im Browser unter$BLUE http://$(curl -s ifconfig.me):5216$NORMAL."
+echo -e "$GREEN-$NORMAL-$GREEN-$NORMAL-$GREEN-$NORMAL-$GREEN-$NORMAL-$GREEN-$NORMAL-$GREEN-$NORMAL-$GREEN-$NORMAL-$GREEN-$NORMAL-$GREEN-$NORMAL-"
+echo -e "$GREEN✓ Installation completed: $NORMAL MySpeed has been installed under $INSTALLATION_PATH."
+echo -e "You can access the web interface in your browser at$BLUE http://$(curl -s ifconfig.me):5216$NORMAL."
 if [ -d "$INSTALLATION_PATH" ]; then
-  echo -e "$BLUEℹ Info:$NORMAL Sollte das Update nicht erfolgreich angewendet worden sein, bitte starte MySpeed mal neu:$BLUE systemctl restart myspeed"
+  echo -e "$BLUEℹ Info:$NORMAL If the update was not successful, please restart MySpeed:$BLUE systemctl restart myspeed"
 fi
-echo -e "$GREEN-$NORMAL-$GREEN-$NORMAL-$GREEN-$NORMAL-$GREEN-$NORMAL-$GREEN-$NORMAL-$GREEN-$NORMAL-$GREEN-$NORMAL-$GREEN-$NORMAL-$GREEN-$NORMAL-" #multicolor
-# MySpeed is installed successfully.
+echo -e "$GREEN-$NORMAL-$GREEN-$NORMAL-$GREEN-$NORMAL-$GREEN-$NORMAL-$GREEN-$NORMAL-$GREEN-$NORMAL-$GREEN-$NORMAL-$GREEN-$NORMAL-$GREEN-$NORMAL-"
