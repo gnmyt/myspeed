25f64306106a68b4458e14bffac78040af02ddef
Mathias Wagner <germannewsmaker@gmail.com>
2024-03-09 17:11:56 +0100

    Created the Imprint.jsx page

---
+++ web/src/pages/Imprint/Imprint.jsx
@@ -0,0 +1,15 @@
+import "./styles.sass";
+
+export const Imprint = () => {
+    return (
+        <div className="imprint-page">
+            <h1>Imprint</h1>
+            <h2>Information in accordance with § 5 TMG</h2>
+
+            <p>Mathias Wagner<br/>c/o IP-Management #14358<br/>Ludwig-Erhard-Str. 18<br/>20459 Hamburg</p>
+
+            <h2>Contact</h2>
+            <p>E-Mail: mathias@gnmyt.dev</p>
+        </div>
+    )
+}
