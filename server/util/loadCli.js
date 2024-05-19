d86ef952827106e23896dedaa19f646406953afb
Mathias Wagner <germannewsmaker@gmail.com>
2024-05-19 15:34:30 +0200

    Implemented both the libre & ookla provider in the loadCli.js util

--- server/util/loadCli.js
+++ server/util/loadCli.js
@@ -1,45 +1,7 @@
-const fs = require('fs');
-const {get} = require('https');
-const decompress = require("decompress");
-const {file} = require("tmp");
-const decompressTarGz = require('decompress-targz');
-const decompressUnzip = require('decompress-unzip');
-const binaries = require('../config/binaries');
-
-const binaryRegex = /speedtest(.exe)?$/;
-const binaryDirectory = __dirname + "/../../bin/";
-const binaryPath = `${binaryDirectory}/speedtest` + (process.platform === "win32" ? ".exe" : "");
-
-const downloadPath = `https://install.speedtest.net/app/cli/ookla-speedtest-${binaries.version}-`;
-
-module.exports.fileExists = async () => fs.existsSync(binaryPath);
-
-module.exports.downloadFile = async () => {
-    const binary = binaries.list.find(b => b.os === process.platform && b.arch === process.arch);
-
-    if (!binary)
-        throw new Error(`Your platform (${process.platform}-${process.arch}) is not supported by the Speedtest CLI`);
-
-    await new Promise((resolve) => {
-        file({postfix: binary.suffix}, async (err, path) => {
-            get(downloadPath + binary.suffix, async resp => {
-                resp.pipe(fs.createWriteStream(path)).on('finish', async () => {
-                    await decompress(path, binaryDirectory, {
-                        plugins: [decompressTarGz(), decompressUnzip()],
-                        filter: file => binaryRegex.test(file.path),
-                        map: file => {
-                            file.path = "speedtest" + (process.platform === "win32" ? ".exe" : "");
-                            return file;
-                        }
-                    });
-                    resolve();
-                });
-            });
-        });
-    });
-}
+const libreProvider = require('./providers/loadLibre');
+const ooklaProvider = require('./providers/loadOokla');
 
 module.exports.load = async () => {
-    if (!await this.fileExists())
-        await this.downloadFile();
+    await libreProvider.load();
+    await ooklaProvider.load();
 }
