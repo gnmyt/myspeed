3f00efa517d3bbe6e53a5103b8ce2f1d9f26a145
Mathias Wagner <germannewsmaker@gmail.com>
2024-05-19 15:34:58 +0200

    The loadServers.js util now loads the nearest ookla & librespeed servers

--- server/util/loadServers.js
+++ server/util/loadServers.js
@@ -1,7 +1,8 @@
 const axios = require('axios');
 const fs = require('fs');
 
-if (!fs.existsSync("data/servers.json")) {
+// Load servers from ookla
+if (!fs.existsSync("data/servers/ookla.json")) {
     let servers = {};
     try {
         axios.get("https://www.speedtest.net/api/js/servers?limit=20")
@@ -12,13 +13,33 @@ if (!fs.existsSync("data/servers.json")) {
                 });
 
                 try {
-                    fs.writeFileSync("data/servers.json", JSON.stringify(servers, null, 4));
+                    fs.writeFileSync("data/servers/ookla.json", JSON.stringify(servers, null, 4));
                 } catch (e) {
-                    console.error("Could not save servers file")
+                    console.error("Could not save servers file");
                 }
             });
     } catch (e) {
         console.error("Could not get servers");
     }
+}
 
+// Load servers from librespeed
+if (!fs.existsSync("data/servers/librespeed.json")) {
+    let servers = {};
+    try {
+        axios.get("https://librespeed.org/backend-servers/servers.php")
+            .then(res => res.data)
+            .then(data => {
+                data?.forEach(row => {
+                    servers[row.id] = row.name;
+                });
+                try {
+                    fs.writeFileSync("data/servers/librespeed.json", JSON.stringify(servers, null, 4));
+                } catch (e) {
+                    console.error("Could not save servers file");
+                }
+            });
+    } catch (e) {
+        console.error("Could not get servers");
+    }
 }
