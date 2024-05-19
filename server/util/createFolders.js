f3fa8d3ba96277eb999ff87fb93f1b63fa62c288
Mathias Wagner <germannewsmaker@gmail.com>
2024-05-19 15:34:11 +0200

    Added the data/servers folder to the createFolders.js util

--- server/util/createFolders.js
+++ server/util/createFolders.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const neededFolder = ["data", "bin", "data/logs"];
+const neededFolder = ["data", "bin", "data/logs", "data/servers"];
 
 neededFolder.forEach(folder => {
     if (!fs.existsSync(folder)) {
