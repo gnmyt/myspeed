ca3e4d29b84661f05874a409cb019b15e38301c6
Mathias Wagner <germannewsmaker@gmail.com>
2024-05-21 14:27:40 +0200

    Created the loadInterfaces.js util

---
+++ server/util/loadInterfaces.js
@@ -0,0 +1,59 @@
+const os = require('os');
+const https = require('https');
+
+let interfacesNode = os.networkInterfaces();
+let interfaces = {};
+
+let usableInterfaces = {};
+
+const requestInterfaces = async () => {
+    console.log("Looking for network interfaces...");
+    for (let i in interfacesNode) {
+        for (let j in interfacesNode[i]) {
+            let address = interfacesNode[i][j];
+
+            if (address.internal) continue;
+
+            let options = {hostname: "speed.cloudflare.com", path: "/__down?bytes=1", method: "GET",
+                family: address.family === "IPv4" ? 4 : 6, timeout: 5000};
+
+            options.agent = new https.Agent(options);
+            options.localAddress = address.address;
+
+            await new Promise((resolve) => {
+
+                const req = https.request(options, () => {
+                    if (!interfaces[i]) interfaces[i] = [];
+                    interfaces[i].push(address.address);
+                    req.destroy();
+                    resolve();
+                });
+
+                req.on('error', () => resolve());
+                req.on('timeout', () => req.destroy());
+
+                req.end();
+            });
+        }
+
+        if (!interfaces[i]) delete interfaces[i];
+    }
+
+    for (let i in interfaces) {
+        for (let j in interfaces[i]) {
+            if (interfaces[i][j].includes(".")) {
+                usableInterfaces[i] = interfaces[i][j];
+                break;
+            }
+        }
+
+        if (!usableInterfaces[i]) usableInterfaces[i] = interfaces[i][0];
+    }
+
+    for (let i in usableInterfaces) {
+        console.log(`Found interface ${i} with IP ${usableInterfaces[i]}`);
+    }
+}
+
+module.exports.requestInterfaces = requestInterfaces;
+module.exports.interfaces = usableInterfaces;
