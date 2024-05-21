d9d52ce5ed46ed44c9d8ea9574a204a7bebaac9b
Mathias Wagner <germannewsmaker@gmail.com>
2024-05-21 15:53:21 +0200

    The config controller now reloads network interfaces when factory reset

--- server/controller/config.js
+++ server/controller/config.js
@@ -187,5 +187,7 @@ module.exports.factoryReset = async () => {
     timer.stopTimer();
     timer.startTimer(configDefaults.cron);
 
+    await interfaces.requestInterfaces();
+
     return true;
 }
