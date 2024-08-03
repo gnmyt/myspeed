e20e83395623320ac76bde6c444243c41285398b
Mathias Wagner <germannewsmaker@gmail.com>
2024-08-03 20:07:50 +0200

    Made div from fragment in Home.jsx

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -2,13 +2,13 @@ import LatestTestComponent from "./components/LatestTest";
 import TestAreaComponent from "./components/TestArea";
 
 const Home = () => (
-    <>
+    <div>
         <LatestTestComponent/>
 
         <br />
 
         <TestAreaComponent/>
-    </>
+    </div>
 )
 
 
